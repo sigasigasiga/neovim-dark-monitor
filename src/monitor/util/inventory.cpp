a96fc0524f1c6c84924a70f5647c77df9c48430a
Yegor Bychin <j@siga.icu>
2023-12-08 01:09:57 +0400

    fix inventory stop

--- src/monitor/util/inventory.cpp
+++ src/monitor/util/inventory.cpp
@@ -27,27 +27,44 @@ private:
 
 inventory_t::inventory_t(boost::asio::any_io_executor exec,
                          std::list<std::unique_ptr<service_t>> services)
-    : exec_{std::move(exec)}, services_{std::move(services)} {}
+    : exec_{std::move(exec)}, state_{std::in_place_type<service_list_t>,
+                                     std::move(services)} {}
 
 void inventory_t::reload() {
-  ranges::for_each(services_, &service_t::reload);
+  ranges::for_each(std::get<service_list_t>(state_), &service_t::reload);
   spdlog::info("The inventory was reloaded");
 }
 
 void inventory_t::stop(std::function<void()> callback) {
-  assert(!stop_callback_);
-  assert(stop_count_ == 0);
+  auto services = std::move(std::get<service_list_t>(state_));
+  state_.emplace<stop_t>(exec_, std::move(services), std::move(callback));
+}
 
-  stop_callback_ = std::move(callback);
+// stop_t
+inventory_t::stop_t::stop_t(boost::asio::any_io_executor exec,
+                            service_list_t &&services,
+                            std::function<void()> callback)
+    : exec_{std::move(exec)},
+      reversed_services_{std::move(services)}, callback_{std::move(callback)} {
+  reversed_services_.reverse();
+  stop();
+}
 
-  for (const auto &service : services_) {
-    if (auto stoppable = dynamic_cast<stoppable_service_t *>(service.get())) {
-      stoppable->stop(stop_callback_t{exec_, stop_callback_, ++stop_count_});
-    }
-  }
+void inventory_t::stop_t::stop() {
+  auto stoppable_it =
+      std::ranges::find_if(reversed_services_, [](const auto &svc_ptr) {
+        return !!dynamic_cast<const stoppable_service_t *>(svc_ptr.get());
+      });
 
-  if (stop_count_ == 0) {
-    boost::asio::post(exec_, std::move(stop_callback_));
+  reversed_services_.erase(reversed_services_.begin(), stoppable_it);
+  if (stoppable_it == reversed_services_.end()) {
+    boost::asio::post(exec_, std::move(callback_));
+  } else {
+    auto &stoppable = static_cast<stoppable_service_t &>(**stoppable_it);
+    stoppable.stop([this, stoppable_it] {
+      reversed_services_.erase(stoppable_it);
+      stop();
+    });
   }
 }
 
--- src/monitor/util/inventory.hpp
+++ src/monitor/util/inventory.hpp
@@ -14,11 +14,25 @@ public:
   void stop(std::function<void()> callback);
 
 private:
-  boost::asio::any_io_executor exec_;
-  std::list<std::unique_ptr<service_t>> services_;
+  using service_list_t = std::list<std::unique_ptr<service_t>>;
 
-  std::uintmax_t stop_count_ = 0;
-  std::function<void()> stop_callback_;
+  class stop_t {
+  public:
+    stop_t(boost::asio::any_io_executor exec, service_list_t &&services,
+           std::function<void()> callback);
+
+  private:
+    void stop();
+
+  private:
+    boost::asio::any_io_executor exec_;
+    service_list_t reversed_services_;
+    std::function<void()> callback_;
+  };
+
+private:
+  boost::asio::any_io_executor exec_;
+  std::variant<service_list_t, stop_t> state_;
 };
 
 } // namespace monitor::util
