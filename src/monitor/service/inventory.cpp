1cb7b9213b3f28978ca305c28db1326eea306ec9
Yegor Bychin <j@siga.icu>
2023-12-31 04:18:37 +0400

    use inventory builder

--- src/monitor/service/inventory.cpp
+++ src/monitor/service/inventory.cpp
@@ -12,22 +12,17 @@ make_inventory(boost::asio::any_io_executor exec,
                request_handler_t::query_t &query,
                neovim_t::delegate_t &neovim_delegate,
                monitor_t::appearance_signal_t appearance_signal) {
-  auto request_handler = std::make_unique<request_handler_t>(query);
-  auto neovim = std::make_unique<neovim_t>(neovim_delegate, *request_handler,
-                                           *request_handler, exec,
-                                           std::move(nvim_socket));
-  auto singleton = std::make_unique<singleton_t>(
-      exec, std::move(singleton_endpoint), *neovim);
-  auto monitor =
-      std::make_unique<monitor_t>(appearance_signal, neovim->clients());
+  util::inventory_builder_t builder{exec};
+  auto &request_handler = builder.add_service<request_handler_t>(query);
+  auto &neovim = builder.add_service<neovim_t>(neovim_delegate, request_handler,
+                                               request_handler, exec,
+                                               std::move(nvim_socket));
+  std::ignore = builder.add_service<singleton_t>(
+      exec, std::move(singleton_endpoint), neovim);
+  std::ignore =
+      builder.add_service<monitor_t>(appearance_signal, neovim.clients());
 
-  std::list<std::unique_ptr<util::service_t>> services;
-  services.push_back(std::move(request_handler));
-  services.push_back(std::move(neovim));
-  services.push_back(std::move(singleton));
-  services.push_back(std::move(monitor));
-
-  return util::inventory_t{exec, std::move(services)};
+  return builder.make_inventory();
 }
 
 } // namespace monitor::service
--- src/monitor/util/inventory.hpp
+++ src/monitor/util/inventory.hpp
@@ -36,4 +36,29 @@ private:
   std::variant<service_list_t, stop_t> state_;
 };
 
+class inventory_builder_t {
+public:
+  inventory_builder_t(boost::asio::any_io_executor exec)
+      : exec_{std::move(exec)} {}
+
+public:
+  template <typename T, typename... Args>
+  [[nodiscard]] T &add_service(Args &&...args) {
+    static_assert(std::convertible_to<T *, service_t *>);
+    auto svc_ptr = std::make_unique<T>(std::forward<Args>(args)...);
+    auto &svc_ref = *svc_ptr;
+    services_.emplace_back(std::move(svc_ptr));
+    return svc_ref;
+  }
+
+  inventory_t make_inventory() {
+    assert(!services_.empty());
+    return inventory_t{std::move(exec_), std::move(services_)};
+  }
+
+private:
+  boost::asio::any_io_executor exec_;
+  std::list<std::unique_ptr<service_t>> services_;
+};
+
 } // namespace monitor::util
